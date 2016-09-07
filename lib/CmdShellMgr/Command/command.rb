0cbdfab18f1c5c4c50986657b80c879e8376a662
Kim, Peter <Peter.Kim@concur.com>
2016-09-06 21:37:46 -0400

    Return class (this) when executed.

--- lib/CmdShellMgr/Command/command.rb
+++ lib/CmdShellMgr/Command/command.rb
@@ -5,29 +5,45 @@ module CmdShellMgr
 
   class Command
 
+    attr_accessor :debug
     attr_accessor :cmd
     attr_accessor :results
     attr_accessor :exitStatus
 
     def initialize(_c=nil)
+      @debug=true
       @exitStatus=nil
+      @results=nil
       @cmd=_c
+    end
+
+    def instance_method
+      self
     end
 
 
     def execute(opts=nil)
       @results=nil
+      @exitStatus=nil
 
-      if @cmd.match(/.*\.(sh|ksh|csh)\s*$/i)
-        puts " execute script  #{@cmd} #{opts.to_s}"
+      begin
         @results = %x[ #{@cmd} #{opts.to_s} ]
-      else
-        @results = %x[ #{@cmd} #{opts.to_s} ]
+        @exitStatus = $?.exitstatus
+      rescue => ex
+        puts __FILE__ + (__LINE__).to_S + " #{ex.class}"
+        puts ex.backtrace
       end
-      @exitStatus = $?.exitstatus
 
-      puts "results => #{@results}"
-      puts "exist   => #{@exitStatus}"
+      if @debug
+        puts __FILE__ + (__LINE__).to_s + " results => #{@results}"
+        puts __FILE__ + (__LINE__).to_s + " exitStatus   => #{@exitStatus}"
+      end
+
+      return self
+    end
+
+    def exitStatus
+      @exitStatus
     end
 
     def results
